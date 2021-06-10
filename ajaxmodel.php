21341aac12b44f64ad9ad9fbc636c22408f5d92c
Azad Choubey <48096790+azadchoubey@users.noreply.github.com>
2021-06-10 17:48:00 +0530

    Add files via upload

--- ajaxmodel.php
+++ ajaxmodel.php
@@ -1,11 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+header('Access-Control-Allow-Origin: *');  
 class ajaxmodel extends CI_Model{
 
 public function checklogin($username){
 
     $q=$this->db->where(['Username'=>$username])
-    ->get('user');
+               ->get('user');
 if($q->num_rows()>0){ 
     
   return true;
