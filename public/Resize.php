ff758fe0abb57e6e6f14742b511d5e48c9ff377b
Azad Choubey <48096790+azadchoubey@users.noreply.github.com>
2021-06-30 18:19:23 +0530

    Add files via upload

---
+++ public/Resize.php
@@ -0,0 +1,166 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Resize extends CI_Controller{
+
+public function index(){
+  
+    $this->load->library('image_lib');
+    $this->load->view('resizeview/resizeimage',array('error' => ' ' ));
+
+}
+public function do_upload()
+{
+
+
+  
+
+    switch ($this->input->post('Width')){
+        case "320":
+        $Height=200;  
+         break;   
+         case "640":
+            $Height=480;  
+             break;   
+             case "800":
+                $Height=600;  
+                 break;   
+                 case "1024":
+                    $Height=768;  
+                     break;   
+                     case "1280":
+                        $Height=768;  
+                         break;   
+            
+                         case "1600":
+                            $Height=1200;  
+                    break;  
+                    default:
+                    $Height=0;    
+    }
+
+
+
+   $Width= $this->input->post('Width');
+  
+   $this->load->helper('file');    
+if(isset($_FILES['image'])){
+
+   $files = $_FILES;    
+        $cpt = count($_FILES['image']['name']);
+        for($i=0; $i<$cpt; $i++)
+        {
+            $_FILES['image']['name']= $files['image']['name'][$i];
+            $_FILES['image']['type']= $files['image']['type'][$i];
+            $_FILES['image']['tmp_name']= $files['image']['tmp_name'][$i];
+            $_FILES['image']['error']= $files['image']['error'][$i];
+            $_FILES['image']['size']= $files['image']['size'][$i];
+
+        $config['upload_path']          = "upload/";
+        $config['allowed_types']        = 'jpg|jpeg|png|gif'; 
+   
+        $config['overwrite']        = true;
+     
+     
+        $this->load->library('upload', $config);
+
+        if ( ! $this->upload->do_upload('image'))
+        {
+                $error = array('error' => $this->upload->display_errors());
+
+                $this->load->view('resizeview/result', $error);
+
+        }
+        else
+        {
+            $data = array('result' => $this->upload->data());
+            
+            $path=$data['result']['full_path'];
+
+                $config['image_library'] = 'GD2';
+                $config['source_image'] =  $path; 
+                $config['create_thumb'] = false;
+                $config['maintain_ratio'] = TRUE;
+                $config['width'] = $Width;
+                $config['height'] = $Height;
+                $config['file_permissions'] = 0777;
+               
+                    $this->load->library('image_lib');
+                    $this->image_lib->initialize($config);
+
+                    $this->image_lib->resize();
+                   
+                    $this->load->view('resizeview/result', $data);
+                   
+        }
+}
+}else{
+    return redirect('resize/resize');
+
+}
+
+}public function pdftojpg(){
+
+  
+
+    if(isset($_FILES['pdf'])){
+        $config['upload_path']          = "upload/pdftojpg";
+        $config['allowed_types']        = 'pdf'; 
+        $config['file_permissions'] = 0777;
+        $config['overwrite']        = true;
+        $this->load->library('upload', $config);
+       
+     
+        if ( ! $this->upload->do_upload('pdf'))
+        {
+                $error = array('error' => $this->upload->display_errors());
+
+                $this->load->view('resizeview/pdftoimage', $error);
+
+        }
+        else
+        {
+            $data = array('result' => $this->upload->data());
+
+            $pdfPath = $config['upload_path'] . '/' .$this->upload->data('file_name');
+           
+          
+            $jpg="upload/pdftojpg/".microtime(true).".jpg";
+ 
+               //make sure that apache has permissions to write in this folder! (common problem)
+            
+            //execute ImageMagick command 'convert' and convert PDF to JPG with applied settings
+            exec('magick convert "'.$pdfPath.'" -resize 1600 "'.$jpg.'"', $output, $return_var);
+            
+         
+            if($return_var == 0) {              //if exec successfuly converted pdf to jpg
+                $img = new imagick( $jpg);
+                $img->setResolution(300, 300);
+                $img->readImage( ); //Open after yuo set resolution.
+                $img->setImageUnits(Imagick::RESOLUTION_PIXELSPERINCH); //Declare the units for resolution.
+                $img->setImageFormat('jpeg');
+                $img->setImageCompression(Imagick::COMPRESSION_JPEG);
+                $img->setImageCompressionQuality(100);
+                $img->writeImage($config['upload_path'] . '/' . str_replace('pdf', 'jpeg', $this->upload->data('file_name')));
+                $img->clear();
+                $img->destroy();
+            }
+            else echo "Conversion failed.<br />".$output;
+    
+            
+           
+            $this->load->view('resizeview/pdftoimage',  $data);
+      
+}
+
+
+
+
+
+}   else{
+
+    $this->load->view('resizeview/pdftoimage');
+}
+
+
+}}
